7df83950905a0a43bca382cbe52b55b956183911
souzza <cauelima752@gmail.com>
2025-10-29 23:31:10 -0300

    adicionando scroll suav com lenis

--- script.js
+++ script.js
@@ -250,3 +250,12 @@ function showNewsletterMessage() {
     message.classList.add("hidden");
   }, 5000);
 }
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
